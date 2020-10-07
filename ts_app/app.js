3fe9a9bd33f96421791fb4a08f84dba301670ffc
Mike Shea <mshea424@yahoo.com>
2020-10-07 17:37:15 -0400

    practiced with basic tsc compiler settings and tsc init

--- ts_app/app.js
+++ ts_app/app.js
@@ -1,13 +1,3 @@
-var userInput;
-var userName;
-userInput = 5;
-userInput = 'mike';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError('An error occured!', 500);
-// const result = generateError('An error occured!', 500)
-// console.log(result)
+"use strict";
+var userName = 'Michael Shea';
+console.log(userName);
--- ts_app/app.ts
+++ ts_app/app.ts
@@ -1,19 +1,3 @@
-let userInput: unknown;
-let userName: string;
+const userName = 'Michael Shea'
 
-userInput = 5
-
-userInput = 'mike'
-
-if (typeof userInput === 'string') {
-    userName = userInput
-}
-
-function generateError(message: string, code: number): never {
-    throw { message: message, errorCode: code }
-}
-
-generateError('An error occured!', 500)
-
-// const result = generateError('An error occured!', 500)
-// console.log(result)
+console.log(userName)
